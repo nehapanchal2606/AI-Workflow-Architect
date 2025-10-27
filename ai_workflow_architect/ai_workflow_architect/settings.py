f7c28a31a438a8437ed478c747df7adb431c455b
Neha Panchal <Nehapanchaltxlab>
2025-10-27 18:04:14 +0530

    Add user model

--- ai_workflow_architect/ai_workflow_architect/settings.py
+++ ai_workflow_architect/ai_workflow_architect/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'customer_app',
+
+
+    # Third party apps
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
